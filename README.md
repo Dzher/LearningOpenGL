6af0540d070995bc0dddd90caa5945360f1e5fe0
Dzher <2539925009@qq.com>
2024-06-28 14:30:15 +0800

    complete the hello window chapter.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-I want learning opengl in just a month, the project record the learning process, go ahead!
+I want learning opengl in just a month, the project record the learning process.
+
+Go ahead!
+
+# Learning Chapter List
+
+`2024-6-28` complete the chapter `hello window` 
--- source/hellowindow/hellowindow.cpp
+++ source/hellowindow/hellowindow.cpp
@@ -2,15 +2,36 @@
 #include <glfw/glfw3.h>
 #include <iostream>
 
+void frameBufferSizeCb(GLFWwindow* window, GLsizei w, GLsizei h)
+{
+    glViewport(0, 0, w, h);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE))
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void render()
+{
+    // status set
+    glClearColor(0.5f, 0.4f, 0.1f, 0.3f);
+    // status use
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main()
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -18,7 +39,7 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if (auto proc_address = (GLADloadproc)glfwGetProcAddress; !gladLoadGLLoader(proc_address))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
@@ -26,8 +47,14 @@ int main()
 
     glViewport(0, 0, 800, 600);
 
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCb);
+
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+        render();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
