0d383ac0c4ead867668aeb43cc6de155a296c79f
Dzher <2539925009@qq.com>
2024-07-19 17:40:26 +0800

    add Mesh class

---
+++ source/utils/mesh/mesh.cpp
@@ -0,0 +1,52 @@
+#include "mesh.h"
+#include <QtGui/qopenglext.h>
+
+using namespace utils;
+
+Mesh::Mesh(const std::vector<Vertex>& vertexes, const std::vector<unsigned int>& indices,
+           const std::vector<Texture>& textures)
+    : vertexes_(vertexes), indices_(indices), textures_(textures)
+{
+    initMesh();
+}
+
+void Mesh::initMesh()
+{
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+
+    glGenBuffers(1, &vbo_);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+    glBufferData(GL_ARRAY_BUFFER, vertexes_.size() * sizeof(Vertex), &vertexes_[0], GL_STATIC_DRAW);
+
+    glGenBuffers(1, &ebo_);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_.size() * sizeof(GLuint), &indices_[0], GL_STATIC_DRAW);
+
+    // vertex Positions
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    // vertex normals
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+    // vertex texture coords
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texture_coord));
+    // vertex tangent
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tangent));
+    // vertex bitangent
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, bitangent));
+    // ids
+    glEnableVertexAttribArray(5);
+    glVertexAttribIPointer(5, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_bone_ids));
+    // weights
+    glEnableVertexAttribArray(6);
+    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_weights));
+    glBindVertexArray(0);
+}
+
+void Mesh::draw(Shader& shader)
+{
+}
---
+++ source/utils/mesh/mesh.h
@@ -0,0 +1,56 @@
+#ifndef _UTILS_MESH_H_
+#define _UTILS_MESH_H_
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <string>
+#include <vector>
+#include "utils/shader/shader.h"
+
+namespace utils
+{
+static constexpr int kMaxBoneInfluence = 4;
+struct Vertex
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texture_coord;
+    glm::vec3 tangent;
+    // bitangent
+    glm::vec3 bitangent;
+    //bone indexes which will influence this vertex
+    int m_bone_ids[kMaxBoneInfluence];
+    //weights from each bone
+    float m_weights[kMaxBoneInfluence];
+};
+
+struct Texture
+{
+    GLuint id;
+    std::string type;
+    std::string path;
+};
+
+class Mesh
+{
+public:
+    explicit Mesh(const std::vector<Vertex>& vertexes, const std::vector<unsigned int>& indices,
+                  const std::vector<Texture>& textures);
+
+    void draw(Shader& shader);
+
+private:
+    void initMesh();
+
+private:
+    std::vector<Vertex> vertexes_;
+    std::vector<unsigned int> indices_;
+    std::vector<Texture> textures_;
+
+    GLuint vao_;
+    GLuint vbo_;
+    GLuint ebo_;
+};
+}  // namespace utils
+#endif
